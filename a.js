75f9ae7b9d3ae74e9d8b31f38d46e54a190648a1
Kawagui <135202943+Kawagui@users.noreply.github.com>
2025-08-15 17:12:12 -0300

    Update a.js

--- a.js
+++ a.js
@@ -1,12 +1,33 @@
-document.body.onload = addElement;
+var i=0;
+//while(i<10){
+
+//document.body.onload = addElement;
+if(document.body!=undefined)document.body.addEventListener("load", addElement());
+
 
 function addElement(){
-    const newDiv = document.createElement("div");
-    const temp = document.createElement("input");
-    const newContent = document.createTextNode("Hi there and greetings!");
-    newDiv.appendChild(temp);
-    newDiv.appendChild(newContent);
-
-    const atual = document.getElementById("container");
-    document.body.insertBefore(newDiv, atual);
+    const input = document.createElement("input");
+    input.type="text";
+    input.maxLength="1";
+    input.className="inputs";
+    var tentativas = document.getElementById("inputs");
+    tentativas.appendChild(input);   
 }
+// addEvent apagar remover caixa de texto (remove)
+// focus para próxima caixa ao digitar
+document.addEventListener('keydown', (e)=>{
+    const teclei=e.key;
+    if(teclei.length===1 && teclei.match(/[a-zA-Z]/) && i<9){
+        i++;
+        const maisUm=addElement();
+        //document.getElementById("inputs").focus();
+    }
+    a();
+});
+
+function a() { // vetor de inputs
+    const inputs = document.querySelectorAll(".inputs");
+    console.log(inputs);
+}
+
+//} 
